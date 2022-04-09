f0ed4a1722a90e67f18c81aea12bd03bc96e6453
monkeywithoutthee <54135971+monkeywithoutthee@users.noreply.github.com>
2022-04-09 20:46:52 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -6,24 +6,31 @@ const app = express();
 
 app.use(cors());
 //app.use(express.static('public'));
+const router = express.Router();
 app.use(bodyParser.json());
 
 
-app.post('/getWordFrom', (req, res) => {
+app.get('/test', function (req, res) {
+//	console.log('getting::');
+  res.send('hello, world!')
+});
+
+app.post('/getWordFrom', function (req, res) {
   var aHeaders = req.headers;
 //  var aBody = req.body;
   //console.log("addLeaderreq:", req);
-//  console.log("TEST headers:", aHeaders);//simple as it gets authorisarion  - expand with need
+//  console.log(req.body,"<<TEST headers:", aHeaders);//simple as it gets authorisarion  - expand with need
   var id = req.params.id;
   var aHeaders = req.headers;
   var aBody = req.body;
+  
   //console.log("addLeaderreq:", req);
-  res.header('Access-Control-Allow-Origin', '*');
+/*  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'POST');
   res.header('Access-Control-Expose-Headers', 'Content-Length');
-  res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
-  console.log("getWordFrom:",req.body);
+  res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');*/
+ // console.log("getWordFrom:",req.body);
 //  console.log("id:", id);
   if (aHeaders.monkey==="spL1shSplAshSploS4"){
       try{
@@ -42,6 +49,7 @@ app.post('/getWordFrom', (req, res) => {
 });
 //.00. . .0 0.0. .*.00. . .0 0.0. .*.00. . .0 0.0. .*.00. . .0 0.0. .*.00. . .0 0.0. .*.00. . .0 0.0. .*
 //m00m m m0 0m0m m^m00m m m0 0m0m m^m00m m m0 0m0m m^m00m m m0 0m0m m^m00m m m0 0m0m m
-app.listen(7766, () => {
-  console.log('Server running on http://localhost:7766')
+app.listen(7777, () => {
+  console.log('Server running on http://localhost:7777');
 });
+
